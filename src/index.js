71ca85c498602c6e5a775be668c89af20352a33f
deepakpal26 <deepak2601200@gmail.com>
2025-07-29 11:51:08 +0530

    final commit application running

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from './GlobalComponents/ThemeProvider';
 import { CartProvider } from 'react-use-cart';
 
-render(
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <ThemeProvider>
       <CartProvider>
@@ -15,7 +16,7 @@ render(
       </CartProvider>
     </ThemeProvider>
   </React.StrictMode>
-, document.getElementById('root'));
+);
 
 
 // If you want to start measuring performance in your app, pass a function
